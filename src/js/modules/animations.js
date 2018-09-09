4512b4e592057232db376943ec5b955592ddac58
stefan <stefanmilic189@gmail.com>
2018-09-09 20:48:54 +0200

    commit

--- src/js/modules/animations.js
+++ src/js/modules/animations.js
@@ -1,32 +1,46 @@
 const $ = require('jquery'),
       isMobile = $(window).outerWidth() < 769 ? true : false;
-      module.exports = function(){
+module.exports = function(){
   //cash var
   let redStripe  = $('.c-reveal--red'),
       grayStripe = $('.c-reveal--gray'),
       statsSlide = $('.b-stats__slide');
+      flyUp = $('.fly-up'),
+      lizingImg = $('.b-liz__media__img'),
+      lizingRedStripe = $('.c-reveal--red-liz'),
+      lizingImgReveal = $('.c-reveal--gray-liz'),
+      lizingTxt = $('.b-liz__intro');
 
   // init controller
   var controller = new ScrollMagic.Controller();
 
-  var tlRed = new TimelineLite();
-  tlRed.to( redStripe, 0.8, {x:105+'%',} )
-       .to( grayStripe, 0.6, {autoAlpha:0} );
+  var tlOnePlace = new TimelineLite();
+  //time line for .b-one-place
+  tlOnePlace.to( redStripe, 0.8, {x:105+'%',} )
+       .to( grayStripe, 0.6, {autoAlpha:0}).delay(0.2);
 
  // build a scene
  var sceneOnePlace = new ScrollMagic.Scene({
    triggerElement: '.b-one-place',
    reverse:false,
-   triggerHook: 0.5,
+   triggerHook: 0.7,
  })
- .setTween(tlRed) // trigger a TweenMax.to tween
- .setClassToggle('yuo')
- .addIndicators({
-		name: 'ONE PLACE',
-		colorTrigger: 'black',
-		indent: 200,
-		colorStart: '#75C695'
-	}) // this requires a plugin
+ .setTween(tlOnePlace) // trigger a TweenMax.to tween
+ .addTo(controller);
+
+//SCENE FOR LIZING
+  var tlLiz = new TimelineMax();
+  //time line for .b-liz
+  tlLiz.to( lizingImgReveal, 0.8, {x:-105+'%'} )
+       .from( lizingTxt, 0.6, {autoAlpha: 0, y:'+=25'} ).delay(0.2);
+
+ // build a scene
+ var sceneOnePlace = new ScrollMagic.Scene({
+   triggerElement: '.c-reveal--gray-liz',
+   reverse:false,
+   triggerHook: 0.7,
+ })
+ .setTween(tlLiz) // trigger a TweenMax.to tween
  .addTo(controller);
 
 $(statsSlide).each(function(index) {
@@ -53,16 +67,52 @@ $(statsSlide).each(function(index) {
   var sceneStat = new ScrollMagic.Scene({
     triggerElement: '.b-stats',
     reverse:false,
-    triggerHook: 0.5,
+    triggerHook: 0.7,
   })
   .setTween(tween) // trigger a TweenMax.to tween
-  .addIndicators({
- 		name: 'STATS',
- 		colorTrigger: 'black',
- 		indent: 200,
- 		colorStart: '#75C695'
- 	})
   .addTo(controller);
 })
 
+$(flyUp).each(function(index) {
+  //scene for all elements tahat have fly in class
+  let self = $(this);
+
+  var tween = TweenMax.from(
+    self,
+    0.6,
+    {autoAlpha: 0,
+      y:'+=25',
+      ease:Linear.easeNone,
+    });
+
+  // build a scene
+  var sceneStat = new ScrollMagic.Scene({
+    triggerElement: this,
+    reverse:false,
+    triggerHook: 0.9,
+  })
+  .setTween(tween) // trigger a TweenMax.to tween
+  .addTo(controller);
+})
+
+//RED STRIP ON LIZING
+var twRedStrip = TweenMax.from(
+  lizingRedStripe,
+  0.35,
+  {width:0,  ease:Linear.easeNone});
+
+// build a scene
+var sceneRedLizingStripe = new ScrollMagic.Scene({
+  triggerElement: '.c-reveal--red-liz',
+  reverse:false,
+  triggerHook: 0.9,
+})
+.setTween(twRedStrip) // trigger a TweenMax.to tween
+// .addIndicators({
+//   name: 'REDLIZ',
+//   colorTrigger: 'black',
+//   indent: 200,
+//   colorStart: '#75C695'
+// })
+.addTo(controller);
 }
--- src/js/modules/local-js.js
+++ src/js/modules/local-js.js
@@ -10,7 +10,9 @@ module.exports = function(){
         $navBurger = $nav.find('.b-header__nav-mob__burger'),
         $navMenu   = $nav.find('.b-header__nav-mob__menu'),
         $navClose  = $navMenu.find('.close'),
-        $navLink   = $nav.find('.b-header__nav-mob__link');
+        $navLink   = $nav.find('.b-header__nav-mob__link'),
+        $footerNav = $('.b-footer__nav'),
+        $footerNavItems = $footerNav.find('.b-footer__nav__item');
 
     $navBurger.on('click', function(){
       //click on burger
@@ -19,13 +21,13 @@ module.exports = function(){
     });
 
     $navClose.on('click', function(){
-      //click on close
+      //click on close main menu
       $navMenu.slideUp(350);
       $('body').css({ overflowY:'visible' });
     });
 
     $navLink.on('click', function(){
-      //click on link in nav
+      //click on link in nav in main menu
       let self = $(this);
 
       if( !( self ).hasClass('active') ){
@@ -33,5 +35,55 @@ module.exports = function(){
         self.addClass('active');
       }
     });
+
+    //FOOTER NAV ON MOBILE
+    $footerNavItems.on('click', function(){
+      let self = $(this),
+          dropDown = self.find('.b-footer__nav__item__links');
+          if(!dropDown.hasClass('open') ){
+            $footerNav.find('.b-footer__nav__item__links.open').removeClass('open').slideUp(300);
+            dropDown.addClass('open').slideDown(300);
+          }else{
+
+          }
+    })
+  }else{
+    //if desktop
+    let $next = $('.c-btn--next'),
+        $bStatsPxFromTop = $('.b-stats').offset().top;
+
+    $next.on('click', function(){
+      //scroll to next section
+      $('html, body').animate({
+          scrollTop: $bStatsPxFromTop
+      }, 500);
+    });
   }
+
+  //for mob and desk
+  //cashing variables
+  var lastScrollTop = 0,
+      $header  = $('header'),
+      $navDesk = $('.b-header__nav-desk');
+
+  $(window).scroll(function(event){
+    //toggle navigation states on scroll
+     var st = $(this).scrollTop();
+     if (st > lastScrollTop){
+         // downscroll code
+         if(st > 50){
+           $header.slideUp(300);
+         }
+     } else {
+        // upscroll code
+        $header.slideDown(300, function(){
+          if(st > 50){
+            $navDesk.addClass('white');
+          }else{
+            $navDesk.removeClass('white');
+          }
+        })
+     }
+     lastScrollTop = st;
+  });
 }
