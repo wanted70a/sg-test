c1bb37843cc39a9ab97f0671b9309f8f0c09a7ab
Stefan Milic <stefan.milic@diwanee.com>
2018-09-10 09:34:14 +0200

    picturefill

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@ const heroSlider =  require('./modules/sliders/hero-slider.js');
 const statsSlider =  require('./modules/sliders/stats-slider.js');
 const localJS =  require('./modules/local-js.js');
 const anim    =  require('./modules/animations.js');
+const picturefill = require('./vendor/picturefill.js')
 
 //cal modules
-window.app = {}
+picturefill();
 window.addEventListener("load", function(event) {
-  console.log("All resources finished loading!");
   heroSlider();
   statsSlider();
   localJS();
